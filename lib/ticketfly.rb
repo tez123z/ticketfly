370bfc790bea95bfeaf76734cfe759d0f70ad6bc
Tom Vestal Evans <tom@sovi.fm>
2015-12-14 13:25:04 -0800

    adjustment for no id

--- lib/ticketfly.rb
+++ lib/ticketfly.rb
@@ -192,6 +192,7 @@ module Ticketfly
       base_uri = "http://www.ticketfly.com/api/events/list.json"
       max_results = 1
       result = JSON.parse(open(base_uri + "?eventId=" + id.to_s).read)
+      return nil if result['events'].first['id'].nil?
       Event.build(result['events'].first)
     end
     
